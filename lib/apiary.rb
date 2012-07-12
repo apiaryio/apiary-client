c14a8831aa6fa45c11a44c23586a660af0e91746
botanicus <stastny@101ideas.cz>
2012-07-12 19:03:01 +0100

    Should works now.

--- lib/apiary.rb
+++
@@ -1,5 +0,0 @@
-# encoding: utf-8
-
-require "nake"
-
-require "apiary/commands/preview"
--- lib/apiary/commands/preview.rb
+++ lib/apiary/commands/preview.rb
@@ -5,26 +5,32 @@
 #   apiary preview my_apib_file.apib
 #   apiary preview [my_apib_file.apib] --api_host=api.apiary.io
 #   apiary preview [my_apib_file.apib] --browser=Safari
+#   apiary preview [my_apib_file.apib] --server
+#   apiary preview [my_apib_file.apib] --server --port=8010
 
 # Display preview of local blueprint file
+
+require "rest_client"
+
 Task.new(:preview) do |task|
   task.description = "Display preview of local blueprint file"
 
   # Configuration.
-  task.config[:apib_file] = "#{File.basename(Dir.pwd)}.apib"
+  task.config[:apib_path] = nil
   task.config[:api_host]  = "api.apiary.io"
   task.config[:headers]   = {accept: "text/html", content_type: "text/plain"}
   task.config[:browser]   = "Google Chrome"
-
-  task.boot do
-    require "rest_client"
-  end
+  task.config[:port]      = 8080
 
   # Singleton methods.
-  def task.validate_apib_file(apib_file)
+  def validate_apib_file(apib_file)
     unless File.exist?(apib_file)
       abort "Apiary definition file hasn't been found: #{apib_file.inspect}"
     end
+  end
+
+  def default_apib_path
+    self.config[:apib_path] || "#{File.basename(Dir.pwd)}.apib"
   end
 
   def browser(options)
@@ -35,30 +41,63 @@ Task.new(:preview) do |task|
     options[:api_host] || self.config[:api_host]
   end
 
-  def resolve_apib_file(apib_file)
-    apib_file = apib_file || self.config[:apib_file]
+  def port(options)
+    options[:port] || config[:port]
+  end
+
+  def rack_app(&block)
+    Rack::Builder.new do
+      run lambda { |env| [200, Hash.new, [block.call]] }
+    end
+  end
+
+  def run_server(apib_path, options)
+    require "rack"
+
+    port = self.port(options)
+
+    app = self.rack_app do
+      host = self.api_host(options)
+      self.query_apiary(host, apib_path)
+    end
+
+    Rack::Server.start(Port: port, app: app)
+  end
+
+  def preview_path(apib_path)
+    basename = File.basename(apib_path)
+    "/tmp/#{basename}-preview.html"
+  end
+
+  def query_apiary(host, apib_path)
+    url  = "https://#{host}/blueprint/generate"
+    data = File.read(apib_path)
+    RestClient.post(url, data, self.config[:headers])
+  end
+
+  def open_generated_page(path, options)
+    sh "open -a '#{self.browser(options)}' '#{path}'"
+  end
+
+  # TODO: Support non OS X systems again. Could be done
+  # through launchy or similar projects, but launchy seemed
+  # to be tad buggy to me (ignored application I wanted to use),
+  # so for now it's done through the open command.
+  def generate_static(apib_path, options)
+    File.open(self.preview_path(apib_path), "w") do |file|
+      file.write(self.query_apiary(self.api_host(options), apib_path))
+      self.open_generated_page(file.path, options)
+    end
   end
 
   # The actual definition.
-  task.define do |apib_file, options|
-    apib_file = self.resolve_apib_file(apib_file)
+  task.define do |apib_path = default_apib_path, options|
+    self.validate_apib_file(apib_path)
 
-    self.validate_apib_file(apib_file)
-
-    url = "https://#{self.api_host(options)}/blueprint/generate"
-    apib = IO.read(apib_file)
-    response = RestClient.post(url, apib, self.config[:headers])
-
-    basename = File.basename(apib_file)
-    File.open("/tmp/#{basename}-preview.html", "w") do |file|
-      file.write(response)
-      # TODO: Support non OS X systems again. Could be done
-      # through launchy or similar projects, but launchy seemed
-      # to be tad buggy to me (ignored application I wanted to use),
-      # so for now it's done through the open command.
-      sh "open -a '#{self.browser(options)}' '#{file.path}'"
+    if options[:server]
+      self.run_server(apib_path, options)
+    else
+      self.generate_static(apib_path, options)
     end
   end
 end
-
-# Task.tasks.default_proc = lambda { |*| Task[:preview] }
