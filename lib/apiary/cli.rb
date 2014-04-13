97fba9827e71cae10fa73692a7b75ceabde41b7c
Honza Javorek <mail@honzajavorek.cz>
2014-04-13 19:35:16 +0200

    Simple validation of command line arguments.

--- lib/apiary/cli.rb
+++ lib/apiary/cli.rb
@@ -7,57 +7,70 @@ module Apiary
 
     def initialize(args)
       options = parse_options!(args)
-      run(args, options)
+      @command = options.delete(:command)
+      run(options)
     end
 
-    def run(args, options)
-      command = args.first || :help
-      command = @command if @command
-      Apiary::Command::Runner.run(command, options)
+    def run(options)
+      Apiary::Command::Runner.run(@command, options)
     end
 
     def parse_options!(args)
-      @command = nil
       options = {}
+      command = nil
+      if args.first && !args.first.start_with?("-")
+        command = args.first
+      end
+
       options_parser = OptionParser.new do |opts|
         opts.on("--path [PATH]") do |path|
+          raise OptionParser::InvalidOption unless ["fetch", "preview", "publish"].include? command
           options[:path] = path
         end
 
         opts.on("--output [PATH]") do |path|
-          options[:output]  = path
+          raise OptionParser::InvalidOption unless ["fetch", "preview"].include? command
+          options[:output] = path
         end
 
         opts.on("--api_host API_HOST") do |api_host|
+          raise OptionParser::InvalidOption unless ["fetch", "preview", "publish"].include? command
           options[:api_host] = api_host
         end
 
         opts.on("--api-name API_HOST") do |api_name|
+          raise OptionParser::InvalidOption unless ["fetch", "publish"].include? command
           options[:api_name] = api_name
         end
 
         opts.on("--browser BROWSER") do |browser|
+          raise OptionParser::InvalidOption if command != "preview"
           options[:browser] = browser
         end
 
         opts.on("--server") do
+          raise OptionParser::InvalidOption if command != "preview"
           options[:server] = true
         end
 
         opts.on("--port [PORT]") do |port|
+          raise OptionParser::InvalidOption unless ["fetch", "preview", "publish"].include? command
           options[:port] = port
         end
 
         opts.on('-v', '--version') do
-          @command = :version
+          raise OptionParser::InvalidOption if command
+          command = :version
         end
 
         opts.on( '-h', '--help') do
-          @command = :help
+          raise OptionParser::InvalidOption if command
+          command = :help
         end
       end
 
       options_parser.parse!
+      options[:command] = command || :help
       options
 
     rescue OptionParser::InvalidOption => e
