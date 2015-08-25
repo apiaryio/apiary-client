0736b4560d2f318a7034ae381e00f43ccc028c33
Jack Repenning <Jack.Repenning@akamai.com>
2015-08-25 11:57:42 -0700

    Include (and verify) all subcommand documentation in README.md

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -6,20 +6,28 @@ describe Apiary::CLI do
     helpcount = Kernel.open('|bin/apiary help') {|f| f.read}.lines.count
     expect(helpcount).to be >= 5
   end
-  
-  it 'includes help in README.md' do
 
-    begin
-      helpcount = Kernel.open('|bin/apiary help | tee help.out') {|f| f.read}.lines.count
-      expect(helpcount).to be >= 5 
+  (["",] +
+   `bin/apiary help`
+     .lines
+     .map {|l| /^ +apiary /.match(l)?l:nil}
+     .map {|l| /^ *apiary help/.match(l)?nil:l}
+     .compact
+     .map {|l| /^ *apiary ([^ ]*)/.match(l)[1] + " " }
+  ).each do |cmd|
 
-      matchcount = `diff -w -C1000 README.md help.out | grep '^ '`.lines.count
-      expect(matchcount).to eq(helpcount)
+    it "includes help #{cmd}in README.md" do
+      begin
+        helpfile="help#{cmd.strip}.out"
+        helptext = open("|bin/apiary help #{cmd} | tee #{helpfile}") {|f| f.read}
+        matchtext = `diff -w -C1000 README.md #{helpfile} | sed -n '/^  \\(.*\\)/s//\\1/p'`
 
-    ensure
-      File.unlink('help.out')
+        expect(matchtext).to eq(helptext)
+
+      ensure
+        !File.exist?(helpfile) || File.unlink(helpfile)
+      end
     end
-
   end
 
 end
