db5a3d9fd34c0d0c79fb3f8ab704cb076da70ace
Ondřej Musil <ondra@apiary.io>
2016-04-27 18:01:43 +0200

    Specs and lib folder has same structure

--- spec/apiary/command/preview_spec.rb
+++ spec/apiary/command/preview_spec.rb
@@ -4,7 +4,7 @@ describe Apiary::Command::Preview do
 
   let(:command) do
     opts = {
-      path: "#{File.expand_path File.dirname(__FILE__)}/../../features/fixtures/apiary.apib"
+      path: "#{File.expand_path File.dirname(__FILE__)}/../../../features/fixtures/apiary.apib"
     }
     Apiary::Command::Preview.new(opts)
   end
