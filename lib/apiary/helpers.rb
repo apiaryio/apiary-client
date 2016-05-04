105659f11283b835ff467d54186dd58c7cd683c4
Ondřej Musil <ondra@apiary.io>
2016-05-04 19:40:09 +0200

    Implementation for Helpers module

--- lib/apiary/helpers.rb
+++ lib/apiary/helpers.rb
@@ -3,11 +3,32 @@
 module Apiary
   module Helpers
     def api_description_source_path(path)
-      puts "Path: #{path}"
+      raise "Invalid path #{path}" unless File.exist? path
+      return path if File.file? path
+      source_path = api_blueprint(path) || swagger(path)
+      return source_path unless source_path.nil?
+      raise 'No API Description source found.'
     end
 
     def api_description_source(path)
-      puts "Path: #{path}"
+      spurce_path = api_description_source_path(path)
+      source = nil
+      File.open(spurce_path, 'r:bom|utf-8') { |file| source = file.read }
+      source
+    end
+
+    protected
+
+    def api_blueprint(path)
+      source_path = File.join(path, 'apiary.apib')
+      return source_path if File.exist? source_path
+      return nil
+    end
+
+    def swagger(path)
+      source_path = File.join(path, 'swagger.yaml')
+      return source_path if File.exist? source_path
+      return nil
     end
   end
 end
